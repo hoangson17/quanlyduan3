573dadd17646badde1b1d078aef51c7ad82ae323
hoangson <tranhoangson03@gmail.com>
2023-12-14 20:58:33 +0700

    first commit

---
+++ WindowsFormsAppQuanly/WindowsFormsAppQuanly/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsAppQuanly
+{
+    internal class Connection
+    {
+        private static string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\THANH AN PC\source\repos\WindowsFormsAppQuanly\WindowsFormsAppQuanly\Database1.mdf"";Integrated Security=True";
+        public static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(stringConnection);
+        }
+    }
+}
